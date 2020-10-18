91f104054c5609ab7fd025729a3ed8e5394eb0be
Lazar Stanojević <laxsrbija@yahoo.com>
2020-10-18 23:48:10 +0200

    Add OpenVPN configuration

---
+++ openvpn/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WEB_SERVER_PORT=6234
+HOST_ADDR="$(curl -s https://api.ipify.org)"
+
+# Build the image
+git clone https://github.com/dockovpn/docker-openvpn.git
+cd docker-openvpn || exit
+docker build \
+  --no-cache \
+  --pull \
+  -t internal-dockovpn:latest .
+
+# Run the container
+docker run -d --cap-add=NET_ADMIN \
+  -p 1194:1194/udp -p $WEB_SERVER_PORT:8080/tcp \
+  -e HOST_ADDR="$HOST_ADDR" \
+  --name dockovpn internal-dockovpn:latest
+
+# Cleanup
+cd .. && rm -rf docker-openvpn
--- youtrack/setup.sh
+++ youtrack/setup.sh
@@ -3,7 +3,10 @@
 EXTERNAL_PORT=8027
 
 # Build the image
-docker build --tag internal-youtrack:latest .
+docker build \
+  --no-cache \
+  --pull \
+  -t internal-youtrack:latest .
 
 # Run the container
 docker run -d -p $EXTERNAL_PORT:8080 --name youtrack internal-youtrack:latest
