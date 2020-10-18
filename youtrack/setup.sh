d9971d185d6ad22348b54e00d5f0a198f7592b28
Lazar Stanojević <laxsrbija@yahoo.com>
2020-10-18 21:32:38 +0200

    Set a default container name

--- youtrack/setup.sh
+++ youtrack/setup.sh
@@ -6,4 +6,4 @@ EXTERNAL_PORT=8027
 docker build --tag internal-youtrack:latest .
 
 # Run the container
-docker run -d -p $EXTERNAL_PORT:8080 internal-youtrack:latest
+docker run -d -p $EXTERNAL_PORT:8080 --name youtrack internal-youtrack:latest
